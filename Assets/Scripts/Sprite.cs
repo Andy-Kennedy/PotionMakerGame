09c43a7f60e82b5546afa506a2dd1c9d6dba8e55
Andrew Kennedy <albinospringroll@aestusstudios.com>
2020-05-07 23:32:25 -0400

    Completed PoC Sprite
    Have a sprite that can move from one point parented on the camera to another, so the sprite moves, but you as the player will always be able to see it.

---
+++ Assets/Scripts/Sprite.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprite : MonoBehaviour
+{
+    public Transform target;
+    public float RotationSpeed = 100f;
+    public float OrbitDegrees = 1f;
+    private Vector3 randomDirection;
+    private void Start()
+    {
+        randomDirection = ChangeRotation();
+    }
+
+    void Update()
+    {
+        transform.Rotate(randomDirection, RotationSpeed * Time.deltaTime);
+        transform.RotateAround(target.position, Vector3.up, OrbitDegrees);
+    }
+
+    Vector3 ChangeRotation()
+    {
+        return new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1));
+    }
+}
---
+++ Assets/Scripts/SpriteRedo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRedo : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, target.localPosition, (speed * Time.deltaTime));
+        if (Vector3.Distance(transform.localPosition, target.localPosition) < .001f)
+        {
+            target.localPosition *= -1.0f;
+        }
+
+    }
+}
